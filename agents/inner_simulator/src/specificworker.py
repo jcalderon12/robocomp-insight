4745a8ebad95f7cdee38dc2c99212f2decc28a40
Jorge Calderon <jcalderoxp@alumnos.unex.es>
2025-12-12 11:54:25 +0100

    URDFs added and basic methods for pybullet implemented

--- agents/inner_simulator/src/specificworker.py
+++ agents/inner_simulator/src/specificworker.py
@@ -25,6 +25,16 @@ from rich.console import Console
 from genericworker import *
 import interfaces as ifaces
 
+import pybullet as p
+import numpy as np
+import locale
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import time
+
+matplotlib.use("TkAgg")
+
 sys.path.append('/opt/robocomp/lib')
 console = Console(highlight=False)
 
@@ -53,37 +63,163 @@ class SpecificWorker(GenericWorker):
             self.timer.timeout.connect(self.compute)
             self.timer.start(self.Period)
 
+        locale.setlocale(locale.LC_NUMERIC, 'en_US.UTF-8')
+
+        # ================ PYBULLET SIMULATION SETUP  ================
+        # ============================================================
+
+        self.physicsClient = p.connect(p.GUI)
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+        p.setGravity(0, 0, -9.81)
+        p.setRealTimeSimulation(1) # Enable real-time simulation
+        p.resetDebugVisualizerCamera(cameraDistance=2.7, cameraYaw=0, cameraPitch=-15,
+                                     cameraTargetPosition=[-1.3, -0.5, 0.2])
+        
+        # ================= PYBULLET MODELS LOADING  ================
+        # ===========================================================
+
+        flags = p.URDF_USE_INERTIA_FROM_FILE
+
+        # LOAD PLANE IN THE SIMULATION
+        self.plane = p.loadURDF("../../etc/URDFs/plane/plane.urdf", basePosition=[0, 0, 0]) 
+
+        # LOAD OBSTACLES IN THE SIMULATION
+        # self.bump_100x5cm = p.loadURDF("./URDFs/bump/bump_100x5cm.urdf", [0, -0.33, 0.001], flags=flags)
+        # self.bump_1000x10cm = p.loadURDF("./URDFs/bump/bump_100x10cm.urdf", [0, -0.33, 0.001], flags=flags)
+        # self.cylinder_bump_10m = p.loadURDF("./URDFs/bump/cylinder_bump_10m.urdf", [0, -0.8, 0.001], p.getQuaternionFromEuler([0, 0, np.pi/2]), flags=flags)
+
+        # LOAD ROBOT IN THE SIMULATION
+        self.robot = p.loadURDF("../../etc/URDFs/shadow/shadow.urdf", [-3.7, -0.7, 0.01], flags=flags)
+
+        time.sleep(0.5)
+
+
+        # ================ ROBOT PARAMETERS  ===============
+        # ==================================================
+
+        self.wheels_radius = 0.1
+        self.distance_between_wheels = 0.44
+        self.distance_from_center_to_wheels = self.distance_between_wheels / 2
+
+        self.motors = ["frame_back_right2motor_back_right", "frame_back_left2motor_back_left", "frame_front_right2motor_front_right", "frame_front_left2motor_front_left"]
+        self.joints_name = self.get_joints_info(self.robot)
+        self.links_name = self.get_link_info(self.robot)
+
+        self.init_pos, self.init_orn = p.getBasePositionAndOrientation(self.robot)
+
+
     def __del__(self):
         """Destructor"""
 
 
     @QtCore.Slot()
     def compute(self):
-        print('SpecificWorker.compute...')
-        # computeCODE
-        # try:
-        #   self.differentialrobot_proxy.setSpeedBase(100, 0)
-        # except Ice.Exception as e:
-        #   traceback.print_exc()
-        #   print(e)
 
-        # The API of python-innermodel is not exactly the same as the C++ version
-        # self.innermodel.updateTransformValues('head_rot_tilt_pose', 0, 0, 0, 1.3, 0, 0)
-        # z = librobocomp_qmat.QVec(3,0)
-        # r = self.innermodel.transform('rgbd', z, 'laser')
-        # r.printvector('d')
-        # print(r[0], r[1], r[2])
-
+        
         return True
 
     def startup_check(self):
         QTimer.singleShot(200, QApplication.instance().quit)
 
+    
+    # =============== PYBULLET MODELS INFO  ================
+    # ======================================================
 
 
+    def get_joints_info(self, robot_id):
+        """
+        Get joint names and IDs from a robot model
 
+        @param robot_id: ID of the robot model in the simulation
+        @return: Dictionary with joint names as keys and joint IDs as values
+        """
+        joint_name_to_id = {}
+        # Get number of joints in the model
+        num_joints = p.getNumJoints(robot_id)
+        # print("Num joints:", num_joints)
 
+        # Populate the dictionary with joint names and IDs
+        for i in range(num_joints):
+            joint_info = p.getJointInfo(robot_id, i)
+            joint_name = joint_info[1].decode("utf-8")
+            joint_name_to_id[joint_name] = i
+            jid = joint_info[0]
+            jtype = joint_info[2]
+            if jtype == p.JOINT_REVOLUTE:
+                p.setJointMotorControl2(bodyUniqueId=robot_id,
+                                        jointIndex=jid,
+                                        controlMode=p.VELOCITY_CONTROL,
+                                        targetVelocity=0,
+                                        force=0)
+        return joint_name_to_id
 
+    def get_link_info(self, robot_id):
+        """
+        Get link names and IDs from a robot model
+
+        @param robot_id: ID of the robot model in the simulation
+        @return: Dictionary with link names as keys and link IDs as values
+        """
+        link_name_to_id = {}
+        # Get number of joints in the model
+        num_links = p.getNumJoints(robot_id)
+        # print("Num links:", num_links)
+
+        # Populate the dictionary with link names and IDs
+        for i in range(num_links):
+            link_info = p.getJointInfo(robot_id, i)
+            link_name = link_info[12].decode("utf-8")
+            link_name_to_id[link_name] = i
+        return link_name_to_id
+    
+    # =============== ROBOT KINEMATICS  ================
+    # ==================================================
+
+    def get_forward_velocity(self):
+        """
+        Get the forward velocity of the robot
+
+        :return: Forward velocity
+        """
+        wheel_velocities = {}
+        for motor_name in self.motors:
+            wheel_velocities[motor_name] = p.getJointState(self.robot, self.joints_name[motor_name])[1]
+        forward_velocity = (wheel_velocities["frame_front_left2motor_front_left"] +
+                            wheel_velocities["frame_front_right2motor_front_right"] +
+                            wheel_velocities["frame_back_left2motor_back_left"] +
+                            wheel_velocities["frame_back_right2motor_back_right"]) * self.wheels_radius / 4
+        return forward_velocity
+
+    def get_angular_velocity(self):
+        """
+        Get the angular velocity of the robot
+
+        :return: Angular velocity
+        """
+        wheel_velocities = {}
+        for motor_name in self.motors:
+            wheel_velocities[motor_name] = p.getJointState(self.robot, self.joints_name[motor_name])[1]
+        angular_velocity = ((wheel_velocities["frame_front_right2motor_front_right"] +
+                            wheel_velocities["frame_back_right2motor_back_right"] -
+                            wheel_velocities["frame_front_left2motor_front_left"] -
+                            wheel_velocities["frame_back_left2motor_back_left"]) * self.wheels_radius /
+                            2 * self.distance_between_wheels)
+        return angular_velocity
+
+    def get_wheels_velocity_from_forward_velocity_and_angular_velocity(self, forward_velocity=0, angular_velocity=0):
+        """
+        Get the velocity of each wheel from the forward velocity of the robot
+
+        :param forward_velocity: Forward velocity of the robot
+        :param angular_velocity: Angular velocity of the robot
+        :return: Dictionary with the velocity of each wheel
+        """
+        wheels_velocity = {
+            "frame_front_left2motor_front_left": forward_velocity / self.wheels_radius - self.distance_from_center_to_wheels * angular_velocity / self.wheels_radius,
+            "frame_front_right2motor_front_right": forward_velocity / self.wheels_radius + self.distance_from_center_to_wheels * angular_velocity / self.wheels_radius,
+            "frame_back_left2motor_back_left": forward_velocity / self.wheels_radius - self.distance_from_center_to_wheels * angular_velocity / self.wheels_radius,
+            "frame_back_right2motor_back_right": forward_velocity / self.wheels_radius + self.distance_from_center_to_wheels * angular_velocity / self.wheels_radius}
+        return wheels_velocity
 
     # =============== DSR SLOTS  ================
     # =============================================
